636d90aedba453e3926b1e57ca958d8ae4f2ce0d
vczb <viniciuszucatti@gmail.com>
2020-10-26 00:32:12 -0300

    add Button support for icons

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,19 @@ export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: JSX.Element
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button = ({
   children,
+  icon,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  ...props
 }: ButtonProps) => (
-  <S.Wrapper fullWidth={fullWidth} size={size}>
+  <S.Wrapper fullWidth={fullWidth} size={size} hasIcon={!!icon} {...props}>
+    {icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -2,16 +2,19 @@ import { screen } from '@testing-library/react'
 
 import Button from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
 
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       padding: '0.8rem 3.2rem',
       height: '4rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the small size', () => {
@@ -39,5 +42,14 @@ describe('<Button />', () => {
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       width: '100%'
     })
+  })
+
+  it('should render a icon version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
+
+    expect(screen.getByText(/buy now/i)).toBeInTheDocument()
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
 })
