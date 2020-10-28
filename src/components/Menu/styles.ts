90ef8e92c9730047559b67a28bb6def50f7cd3ab
vczb <viniciuszucatti@gmail.com>
2020-10-27 22:32:19 -0300

    add animation to Menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -53,6 +53,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
     right: 0;
     height: 100vh;
     overflow: hidden;
+    transition: opacity 0.3s ease-in-out;
     opacity: ${isOpen ? 1 : 0};
     pointer-events: ${isOpen ? 'all' : 'none'};
 
@@ -77,6 +78,12 @@ export const MenuFull = styled.nav<MenuFullProps>`
       font-weight: ${theme.font.bold};
       font-size: ${theme.font.sizes.xlarge};
       margin-bottom: ${theme.spacings.small};
+      transform: ${isOpen ? 'translate(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
+    }
+    ${RegisterBox}{
+      transform: ${isOpen ? 'translate(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
     }
   `}
 `
