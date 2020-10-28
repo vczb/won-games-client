0da143a58575001cf3f5d23e24fd985008f60f4b
vczb <viniciuszucatti@gmail.com>
2020-10-27 22:26:03 -0300

    add styles to Menu

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,9 +1,17 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Menu from '.'
+import media from 'styled-media-query'
 
 export default {
   title: 'Menu',
-  component: Menu,
+  component: Menu
 } as Meta
 
 export const Default: Story = (args) => <Menu />
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
