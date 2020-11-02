156a93ce8e50e6107d6e9eeb6816d285c4e2c752
vczb <viniciuszucatti@gmail.com>
2020-11-01 22:14:16 -0300

    add styles from banner

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -12,7 +12,7 @@ const props = {
 
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    renderWithTheme(<Banner {...props} />)
+    const { container } = renderWithTheme(<Banner {...props} />)
 
     expect(
       screen.getByRole('heading', { name: 'Defy death' })
@@ -23,5 +23,7 @@ describe('<Banner />', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
