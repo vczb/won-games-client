9e6dfab7bac5b673159b46dae793b92db894fa27
vczb <viniciuszucatti@gmail.com>
2020-11-09 22:33:24 -0300

    add component BannerSlider

---
+++ src/components/BannerSlider/test.tsx
@@ -0,0 +1,58 @@
+import 'match-media-mock'
+
+import { render, screen } from '@testing-library/react'
+
+import BannerSlider from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
+describe('<BannerSlider />', () => {
+  it('should render vertical slider', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+  })
+
+  it('should render whit 1 active item', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(2)
+    expect(container.querySelectorAll('li.slick-active')).toHaveLength(1)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death 1/i, hidden: false })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /defy death 2/i, hidden: true })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the dots', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelector('ul.slick-dots')).toBeInTheDocument()
+  })
+
+  it('should match to snapshot', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
