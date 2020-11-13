af7e7d7b2f7f9dc03162337f3d3ed5aab67ae80b
vczb <viniciuszucatti@gmail.com>
2020-11-12 22:42:48 -0300

    add first sections to Home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,9 +1,44 @@
+import { Container } from 'components/Container'
+import Menu from 'components/Menu'
+import Footer from 'components/Footer'
+
 import * as S from './styles'
+import Heading from 'components/Heading'
 
 const Home = () => (
-  <S.Wrapper>
-    <h1>Home</h1>
-  </S.Wrapper>
+  <section>
+    <Container>
+      <Menu></Menu>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        News
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft color="white" lineColor="secondary">
+        Most Popular
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft color="white" lineColor="secondary">
+        Upcomming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft color="white" lineColor="secondary">
+        Free Games
+      </Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
 )
 
 export default Home
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,14 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Home from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<Home />', () => {
-  it('should render the reading', () => {
-    const { container } = render(<Home />)
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home />)
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /contact us/i })
+    ).toBeInTheDocument()
   })
 
+  it('should render the sections', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Most Popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Free Games/i })
+    ).toBeInTheDocument()
+  })
 })
