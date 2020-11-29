f7e8c8806ff4e4d1b0de899cc9e3026adfd8b992
vczb <viniciuszucatti@gmail.com>
2020-11-29 01:06:04 -0300

    groups component as Game in the storybook

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import GameInfo, { GameInfoProps } from '.'
 import mockGame from './mock'
 export default {
-  title: 'GameInfo',
+  title: 'Game/GameInfo',
   component: GameInfo,
   args: mockGame,
   parameters: {
