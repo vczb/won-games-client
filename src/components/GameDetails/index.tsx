6bafacd1d146efd63cdd580f6c02e3becd774e53
vczb <viniciuszucatti@gmail.com>
2020-11-29 20:14:48 -0300

    add component GameDetails

---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,82 @@
+import { Apple, Windows, Linux } from '@styled-icons/fa-brands'
+
+import Heading from 'components/Heading'
+import MediaMatch from 'components/MediaMatch'
+
+import * as S from './styles'
+
+type Platform = 'windows' | 'linux' | 'mac'
+
+type Rating = 'BR0' | 'BR10' | 'BR12' | 'BR14' | 'BR16' | 'BR18'
+
+export type GameDetailsProps = {
+  developer: string
+  releaseDate: string
+  rating: Rating
+  platforms: Platform[]
+  genres: string[]
+}
+
+const GameDetails = ({
+  platforms,
+  developer,
+  releaseDate,
+  rating,
+  genres
+}: GameDetailsProps) => {
+  const platformIcons = {
+    linux: <Linux title="linux" size={18} />,
+    mac: <Apple title="mac" size={18} />,
+    windows: <Windows title="windows" size={18} />
+  }
+  return (
+    <S.Wrapper>
+      <MediaMatch greaterThan="small">
+        <Heading color="white" lineLeft lineColor="secondary">
+          Game Details
+        </Heading>
+      </MediaMatch>
+
+      <S.Content>
+        <S.Block>
+          <S.Label>Developer</S.Label>
+          <S.Description>{developer}</S.Description>
+        </S.Block>
+        <S.Block>
+          <S.Label>Release Date</S.Label>
+          <S.Description>
+            {new Intl.DateTimeFormat('en-us', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(releaseDate))}
+          </S.Description>
+        </S.Block>
+        <S.Block>
+          <S.Label>Platforms</S.Label>
+          <S.IconsWrapper>
+            {platforms.map((icon: Platform) => (
+              <S.Icon key={icon}>{platformIcons[icon]}</S.Icon>
+            ))}
+          </S.IconsWrapper>
+        </S.Block>
+        <S.Block>
+          <S.Label>Publisher</S.Label>
+          <S.Description>2k</S.Description>
+        </S.Block>
+        <S.Block>
+          <S.Label>Rating</S.Label>
+          <S.Description>
+            {rating === 'BR0' ? 'FREE' : `${rating.replace('BR', '')}+`}
+          </S.Description>
+        </S.Block>
+        <S.Block>
+          <S.Label>Genres</S.Label>
+          <S.Description>{genres.join(' / ')}</S.Description>
+        </S.Block>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default GameDetails
---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,68 @@
+import { screen } from '@testing-library/react'
+
+import GameDetails, { GameDetailsProps } from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const props: GameDetailsProps = {
+  developer: 'Different Tales',
+  platforms: ['windows', 'mac', 'linux'],
+  releaseDate: '2020-11-21T23:00:00',
+  rating: 'BR0',
+  genres: ['Role-playing', 'Narrative']
+}
+
+describe('<GameDetails />', () => {
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /developer/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /release date/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /publisher/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+  })
+
+  it('should render platform icons', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByRole('img', { name: /linux/ })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /windows/ })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /mac/ })).toBeInTheDocument()
+  })
+
+  it('should render the formated date', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
+  })
+
+  it('should render free rating when BR0', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/FREE/i)).toBeInTheDocument()
+  })
+
+  it('should render 18 plus rating when BR18', () => {
+    renderWithTheme(<GameDetails {...props} rating="BR18" />)
+
+    expect(screen.getByText(/18\+/i)).toBeInTheDocument()
+  })
+
+  it('should render a list of genres', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/Role-playing \/ Narrative/i)).toBeInTheDocument()
+  })
+})
