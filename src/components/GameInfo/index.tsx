b69d9427204feb2f002edec1be7a88492c9dbbf7
vczb <viniciuszucatti@gmail.com>
2020-11-29 00:40:14 -0300

    add component GameInfo

---
+++ src/components/GameInfo/index.tsx
@@ -0,0 +1,34 @@
+import * as S from './styles'
+import Heading from 'components/Heading'
+import Ribbon from 'components/Ribbon'
+import Button from 'components/Button'
+import {
+  AddShoppingCart,
+  FavoriteBorder
+} from '@styled-icons/material-outlined'
+
+export type GameInfoProps = {
+  title: string
+  description: string
+  price: string
+}
+
+const GameInfo = ({ title, description, price }: GameInfoProps) => (
+  <S.Wrapper>
+    <Heading lineBottom color="black">
+      {title}
+    </Heading>
+    <Ribbon color="secondary">${`$${price}`}</Ribbon>
+    <S.Description>{description}</S.Description>
+    <S.ButtonsWrapper>
+      <Button icon={<AddShoppingCart />} size="large">
+        Add to cart
+      </Button>
+      <Button icon={<FavoriteBorder />} size="large" minimal>
+        Wishlist
+      </Button>
+    </S.ButtonsWrapper>
+  </S.Wrapper>
+)
+
+export default GameInfo
---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import GameInfo from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const props = {
+  title: 'Game Title',
+  description: 'Game Description',
+  price: '200,00'
+}
+
+describe('<GameInfo />', () => {
+  it('should render game informations', () => {
+    const { container } = renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /game title/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/game description/i)).toBeInTheDocument()
+    expect(screen.getByText(/\$200/)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
+  })
+})
