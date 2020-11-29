87b9ecd361b49adaedb024cb57e749fe564d2f44
vczb <viniciuszucatti@gmail.com>
2020-11-29 20:49:40 -0300

    add component TextContent

---
+++ src/components/TextContent/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+import Heading from 'components/Heading'
+
+export type TextContentProps = {
+  title?: string
+  content: string
+}
+
+const TextContent = ({ title, content }: TextContentProps) => (
+  <S.Wrapper>
+    {!!title && (
+      <Heading lineLeft color="black" lineColor="secondary">
+        {title}
+      </Heading>
+    )}
+
+    <div dangerouslySetInnerHTML={{ __html: content }} />
+  </S.Wrapper>
+)
+
+export default TextContent
