a36843f8c1e5328f1147e19c554c24f105f15443
vczb <viniciuszucatti@gmail.com>
2020-11-29 20:46:19 -0300

    export style WrapperModifiers from Heading

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -2,7 +2,7 @@ import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 import { HeadingProps, lineColors } from '.'
 
-const WrapperModifiers = {
+export const WrapperModifiers = {
   small: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.medium};
     &::after {
