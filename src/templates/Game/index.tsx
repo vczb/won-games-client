7414358c73fe0777597bf717f507be6cd3a6974a
vczb <viniciuszucatti@gmail.com>
2020-12-01 15:42:23 -0300

    add template Game

---
+++ src/templates/Game/index.tsx
@@ -0,0 +1,15 @@
+import Base from 'templates/Base'
+
+import * as S from './styles'
+
+export type GameTemplateProps = {
+  cover: string
+}
+
+const Game = ({ cover }: GameTemplateProps) => (
+  <Base>
+    <S.Cover src={cover} role="image" aria-label="cover" />
+  </Base>
+)
+
+export default Game
---
+++ src/templates/Game/test.tsx
@@ -0,0 +1,11 @@
+import {} from '@testing-library/react'
+
+// import Game from '.'
+
+describe('<Game />', () => {
+  it('should render the reading', () => {
+    // render(<Game cover={''} />)
+    // expect(screen.getByRole('heading', { name: /Game/i })).toBeInTheDocument()
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+})
