386808d94c1253b079d7bd8f758842ae7934316a
vczb <viniciuszucatti@gmail.com>
2020-12-01 22:55:21 -0300

    add Heading to Wishlist page

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,17 @@
+import Base from 'templates/Base'
+
+// import * as S from './styles'
+import { Container } from 'components/Container'
+import Heading from 'components/Heading'
+
+const Wishlist = () => (
+  <Base>
+    <Container>
+      <Heading color="white" lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+    </Container>
+  </Base>
+)
+
+export default Wishlist
