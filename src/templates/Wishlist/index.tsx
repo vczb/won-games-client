c92424569dc7e2dcc1f2fc42d466615478de1131
vczb <viniciuszucatti@gmail.com>
2020-12-01 23:05:35 -0300

    add recommended games to Wishlist page

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,16 +1,33 @@
 import Base from 'templates/Base'
 
-// import * as S from './styles'
-import { Container } from 'components/Container'
 import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
 
-const Wishlist = () => (
+// import * as S from './styles'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading color="white" lineLeft lineColor="secondary">
         Wishlist
       </Heading>
     </Container>
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 )
 
