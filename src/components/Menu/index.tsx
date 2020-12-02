aa1e1df2ff89921bdd0649b8fce6b4c4a2edaed8
vczb <viniciuszucatti@gmail.com>
2020-12-01 22:41:40 -0300

    add Links to Home on Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -27,13 +27,19 @@ const Menu = ({ username }: MenuProps) => {
       </MediaMatch>
 
       <S.LogoWrapper>
-        <Logo hideOnMobile />
+        <Link href="/" passHref>
+          <a>
+            <Logo hideOnMobile />
+          </a>
+        </Link>
       </S.LogoWrapper>
 
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink href="#">Home</S.MenuLink>
+          </Link>
+          <S.MenuLink>Explore</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
 
@@ -56,7 +62,9 @@ const Menu = ({ username }: MenuProps) => {
       <S.MenuFull isOpen={isOpen} aria-hidden={!isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
           {!!username && (
             <>
