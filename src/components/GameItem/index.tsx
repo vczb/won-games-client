b2f136d01922ca3d0fa812a57380680da198eb5d
vczb <viniciuszucatti@gmail.com>
2020-12-20 17:26:02 -0300

    add component GameItem

---
+++ src/components/GameItem/index.tsx
@@ -0,0 +1,23 @@
+import * as S from './styles'
+
+export type GameItemProps = {
+  img: string
+  title: string
+  price: string
+}
+
+const GameItem = ({ img, price, title }: GameItemProps) => (
+  <S.Wrapper>
+    <S.GameContent>
+      <S.ImageBox>
+        <img src={img} alt={title} />
+      </S.ImageBox>
+      <S.Content>
+        <S.Title>{title}</S.Title>
+        <S.Price>{price}</S.Price>
+      </S.Content>
+    </S.GameContent>
+  </S.Wrapper>
+)
+
+export default GameItem
