b93f3888f4e2e294ca91f45e70a8e0927c37b70a
vczb <viniciuszucatti@gmail.com>
2021-01-19 21:43:02 -0300

    add PaymentOptions styles

--- src/components/PaymentOptions/test.tsx
+++ src/components/PaymentOptions/test.tsx
@@ -1,11 +1,15 @@
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import PaymentOptions from '.'
+import cards from './mock'
 
 describe('<PaymentOptions />', () => {
-  it('should render the PaymentOptions', () => {
-    const { container } = render(<PaymentOptions handlePlayment={() => ({})} />)
+  it('should render the saved card options and the add new card button', () => {
+    renderWithTheme(<PaymentOptions cards={cards} handlePayment={jest.fn} />)
 
-    expect(container.firstChild).toBeInTheDocument()
+    expect(screen.getByLabelText(/4325/)).toBeInTheDocument()
+    expect(screen.getByLabelText(/4326/)).toBeInTheDocument()
+    expect(screen.getByText(/add a new credit card/i)).toBeInTheDocument()
   })
 })
