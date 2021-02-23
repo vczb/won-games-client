538fc6c3a4114b831e68da439f43b0d1272078dc
vczb <viniciuszucatti@gmail.com>
2021-02-22 22:27:36 -0300

    add CardsList component

---
+++ src/components/CardsList/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react'
+
+import CardsList from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+import cardsMock from 'components/PaymentOptions/mock'
+
+describe('<CardsList />', () => {
+  it('should render the cards list', () => {
+    renderWithTheme(<CardsList cards={cardsMock} />)
+
+    expect(
+      screen.getByRole('heading', { name: /my cards/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /visa/i })).toHaveAttribute(
+      'src',
+      '/img/visa.png'
+    )
+    expect(screen.getByRole('img', { name: /mastercard/i })).toHaveAttribute(
+      'src',
+      '/img/master-card.png'
+    )
+
+    expect(screen.getByText(/4325/gi)).toBeInTheDocument()
+    expect(screen.getByText(/4326/gi)).toBeInTheDocument()
+  })
+})
