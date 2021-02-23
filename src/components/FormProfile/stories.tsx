368cce23e5d11c7a9a933e07b2bf058e7dafd8f6
vczb <viniciuszucatti@gmail.com>
2021-02-22 21:20:32 -0300

    add FormProfile styles

--- src/components/FormProfile/stories.tsx
+++ src/components/FormProfile/stories.tsx
@@ -2,8 +2,12 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import FormProfile from '.'
 
 export default {
-  title: 'FormProfile',
-  component: FormProfile,
+  title: 'Form/FormProfile',
+  component: FormProfile
 } as Meta
 
-export const Default: Story = (args) => <FormProfile />
+export const Default: Story = () => (
+  <div style={{ maxWidth: 860, margin: 'auto' }}>
+    <FormProfile />
+  </div>
+)
