c2f0025f35a565f4393a6c6e6741dc02e07abdd7
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-03-27 21:48:34 -0300

    fix revalidate moving to outside and using no-cache from Apollo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,13 @@ export async function getStaticProps() {
     data: { banners, newGames, upcommingGames, freeGames, sections }
   } = await apolloClient.query<QueryHome, QueryHomeVariables>({
     query: QUERY_HOME,
-    variables: { date: TODAY }
+    variables: { date: TODAY },
+    fetchPolicy: 'no-cache'
   })
 
   return {
+    revalidate: 10,
     props: {
-      revalidate: 60,
       banners: bannerMapper(banners),
       newGames: gamesMapper(newGames),
       newGamesTitle: sections?.newGames?.title,
