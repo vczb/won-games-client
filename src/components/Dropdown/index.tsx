46e02326d81e064a2ab3341ac9d10f395387aa48
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-03-31 19:53:32 -0300

    add overlay to Dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -12,6 +12,7 @@ const Dropdown = ({ children, title }: DropdownProps) => {
     <S.Wrapper isOpen={isOpen}>
       <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
       <S.Content aria-hidden={!isOpen}>{children}</S.Content>
+      <S.Overlay onClick={() => setIsOpen(!isOpen)} aria-hidden={!isOpen} />
     </S.Wrapper>
   )
 }
