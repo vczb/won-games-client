dc3a508d49612226315ffe51bef37026bc8671f0
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-04-12 20:00:20 -0300

    add Context to CartList

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,40 +1,42 @@
 import Link from 'next/link'
 import Button from 'components/Button'
-import GameItem, { GameItemProps } from 'components/GameItem'
+import GameItem from 'components/GameItem'
 import * as S from './styles'
 import Empty from 'components/Empty'
+import { useCart } from 'hooks/use-cart'
 
 export type CartListProps = {
-  items?: GameItemProps[]
-  total?: string
   hasButton?: boolean
 }
 
-const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
-  <S.Wrapper isEmpty={!items.length}>
-    {items.length ? (
-      <>
-        {items.map((item) => (
-          <GameItem key={item.title} {...item} />
-        ))}
-        <S.Footer>
-          {!hasButton && <span>Total: </span>}
-          <S.Total> {total}</S.Total>
-          {hasButton && (
-            <Link href="/cart">
-              <Button as="a">Buy it now</Button>
-            </Link>
-          )}
-        </S.Footer>
-      </>
-    ) : (
-      <Empty
-        hasLink
-        title="Your cart is empty"
-        description="Go back to the store and explore great games and offers."
-      />
-    )}
-  </S.Wrapper>
-)
+const CartList = ({ hasButton = false }: CartListProps) => {
+  const { items, total } = useCart()
+  return (
+    <S.Wrapper isEmpty={!items.length}>
+      {items.length ? (
+        <>
+          {items.map((item) => (
+            <GameItem key={item.title} {...item} />
+          ))}
+          <S.Footer>
+            {!hasButton && <span>Total: </span>}
+            <S.Total> {total}</S.Total>
+            {hasButton && (
+              <Link href="/cart">
+                <Button as="a">Buy it now</Button>
+              </Link>
+            )}
+          </S.Footer>
+        </>
+      ) : (
+        <Empty
+          hasLink
+          title="Your cart is empty"
+          description="Go back to the store and explore great games and offers."
+        />
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default CartList
--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -1,14 +1,19 @@
 import { screen, render } from 'utils/testUtils'
 
-import mockItems from './mock'
+import items from './mock'
 
 import CartList from '.'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 
 describe('<CartList />', () => {
   it('should render the card list correctly', () => {
-    const { container } = render(
-      <CartList items={mockItems} total="R$ 330,00" />
-    )
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      total: 'R$ 330,00'
+    }
+
+    const { container } = render(<CartList />, { cartProviderProps })
 
     expect(screen.getAllByRole('heading')).toHaveLength(2)
     expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' })
@@ -17,7 +22,11 @@ describe('<CartList />', () => {
   })
 
   it('should render the button', () => {
-    render(<CartList items={mockItems} total="R$ 330,00" hasButton />)
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items
+    }
+    render(<CartList hasButton />, { cartProviderProps })
 
     expect(screen.getByText(/Buy it now/i)).toBeInTheDocument()
   })
