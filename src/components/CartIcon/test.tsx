e76e2e564e8c16e2c3e17ef13f7196616975039b
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-04-12 19:39:11 -0300

    update CartIcon to use useCart hook

--- src/components/CartIcon/test.tsx
+++ src/components/CartIcon/test.tsx
@@ -1,3 +1,4 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { screen, render } from 'utils/testUtils'
 
 import CartIcon from '.'
@@ -10,14 +11,10 @@ describe('<CartIcon />', () => {
   })
 
   it('should render with badge', () => {
-    render(<CartIcon quantity={3} />)
+    render(<CartIcon />, {
+      cartProviderProps: { ...CartContextDefaultValues, quantity: 3 }
+    })
     expect(screen.getByLabelText(/Cart Items/i)).toBeInTheDocument()
     expect(screen.getByText(/3/)).toBeInTheDocument()
-  })
-
-  it('should render with badge only if has positive numbers', () => {
-    render(<CartIcon quantity={-1} />)
-    expect(screen.queryByLabelText(/Cart Items/i)).not.toBeInTheDocument()
-    expect(screen.queryByLabelText(/-1/i)).not.toBeInTheDocument()
   })
 })
