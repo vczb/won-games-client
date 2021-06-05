a36a71b50f80e24d76f8f906796847f82a49a159
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-06-05 20:05:49 -0300

    add validations to reset and forgot fields

--- src/components/FormResetPassword/index.tsx
+++ src/components/FormResetPassword/index.tsx
@@ -7,12 +7,12 @@ import { ErrorOutline, Lock } from '@styled-icons/material-outlined'
 import { FormWrapper, FormLoading, FormError } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
-import { FieldErrors } from 'utils/validations'
+import { FieldErrors, resetValidate } from 'utils/validations'
 
 const FormResetPassword = () => {
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
-  const [values, setValues] = useState({ password: '', confirmPassword: '' })
+  const [values, setValues] = useState({ password: '', confirm_password: '' })
   const [loading, setLoading] = useState(false)
   const routes = useRouter()
   const { push, query } = routes
@@ -25,7 +25,7 @@ const FormResetPassword = () => {
     event.preventDefault()
     setLoading(true)
 
-    const errors = {} //TODO: validate
+    const errors = resetValidate(values)
 
     if (Object.keys(errors).length) {
       setFieldError(errors)
