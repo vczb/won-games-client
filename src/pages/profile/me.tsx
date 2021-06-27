33dbd01fcb836316350544d028cc719fb33e2ffa
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-06-27 19:14:55 -0300

    fix profile login cached session

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,10 +1,15 @@
 import { GetServerSidePropsContext } from 'next'
-import FormProfile, { FormProfileProps } from 'components/FormProfile'
-import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
-import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
+
 import Profile from 'templates/Profile'
-import { initializeApollo } from 'utils/apollo'
+import FormProfile, { FormProfileProps } from 'components/FormProfile'
+
 import protectedRoutes from 'utils/protectedRoutes'
+import { initializeApollo } from 'utils/apollo'
+import {
+  QueryProfileMe,
+  QueryProfileMeVariables
+} from 'graphql/generated/QueryProfileMe'
+import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
 
 export default function Me(props: FormProfileProps) {
   return (
@@ -18,11 +23,17 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
   const apolloClient = initializeApollo(null, session)
 
-  const { data } = await apolloClient.query<QueryProfileMe>({
-    query: QUERY_PROFILE_ME
+  const { data } = await apolloClient.query<
+    QueryProfileMe,
+    QueryProfileMeVariables
+  >({
+    query: QUERY_PROFILE_ME,
+    variables: {
+      identifier: session?.id as string
+    }
   })
 
   return {
-    props: { session, username: data.me?.username, email: data.me?.email }
+    props: { session, username: data.user?.username, email: data.user?.email }
   }
 }
