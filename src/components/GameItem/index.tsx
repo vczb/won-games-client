e1c4bc4268d0edf12b0a5a3dddcf1fb1b670fc43
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-10-31 13:03:42 -0300

    fix OrdersList tests

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -4,8 +4,8 @@ import * as S from './styles'
 
 export type PaymentInfoProps = {
   number: string
-  flag: string
-  img: string
+  flag: string | null
+  img: string | null
   purchaseDate: string
 }
 
@@ -59,7 +59,7 @@ const GameItem = ({
           <p>{paymentInfo.purchaseDate}</p>
           <S.CardInfo>
             <span>{paymentInfo.number}</span>
-            {!!paymentInfo.img && <img src={paymentInfo.img} alt={paymentInfo.flag} />}
+            {!!paymentInfo.img && !!paymentInfo.flag && <img src={paymentInfo.img} alt={paymentInfo.flag} />}
           </S.CardInfo>
         </S.PaymentContent>
       )}
