d8714b2b684864478d0e7d7fd4866fc1c6f5760d
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-11-02 20:45:01 -0300

    update GameCard to use next/image

--- src/components/GameCard/styles.ts
+++ src/components/GameCard/styles.ts
@@ -12,7 +12,8 @@ export const Wrapper = styled.article`
 `
 
 export const ImageBox = styled.a`
-  height: 14rem;
+  position: relative;
+  min-height: 14rem;
   width: 100%;
   background: #f6f7f8;
   background-image: linear-gradient(
@@ -24,11 +25,7 @@ export const ImageBox = styled.a`
   );
   background-size: 80rem 14rem;
   animation: placeholderShimmer 1s linear infinite forwards;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+
   @keyframes placeholderShimmer {
     0% {
       background-position: -40rem 0;
