de560cce8f184feaf050044a00cebb472f374eb3
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-11-02 19:16:10 -0300

    configure Banner to use next/image

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import * as S from './styles'
 import Button from 'components/Button'
 import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
@@ -29,7 +30,10 @@ const Banner = ({
         {ribbon}
       </Ribbon>
     )}
-    <S.Image src={img} role="img" aria-label={title} />
+    <S.ImageWrapper >
+      <Image src={img} alt={title} layout="fill" objectFit="cover" />
+
+    </S.ImageWrapper >
     <S.Caption>
       <S.Title>{title}</S.Title>
       <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
