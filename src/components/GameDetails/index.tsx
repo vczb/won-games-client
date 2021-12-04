332838a3371bf26b1209866b67761ef1b1c909e6
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-04 19:23:47 -0300

    search for details and showcase inside Game Page

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -32,7 +32,7 @@ const GameDetails = ({
     windows: <Windows title="windows" size={18} />
   }
   return (
-    <S.Wrapper>
+    <S.Wrapper data-cy="game-details">
       <MediaMatch greaterThan="small">
         <Heading color="white" lineLeft lineColor="secondary">
           Game Details
