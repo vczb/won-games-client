bb14feb6f98fe6f9c1da1c8fff6e1ba8c16269c2
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-04 10:37:12 -0300

    config Cypress TS

---
+++ cypress/integration/example.ts
@@ -0,0 +1,7 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Cypress TS', () => {
+  it('Should go to google', () => {
+    cy.google()
+  })
+})
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,12 @@
+// load type definitions that come with Cypress module
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /*
+     * Custom command to visit google page
+     * @example cy.google()
+     */
+    google(): Chainable<Window>
+  }
+}
