be4698d4edcf6010c8675b79b678a2dff7a0febd
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-04 17:07:22 -0300

    add start-server-and-test to Cypress

--- cypress/e2e/example.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../support/index.d.ts" />
-
-describe('Cypress TS', () => {
-  it.skip('Should go to google', () => {
-    cy.google()
-  })
-})
---
+++ cypress/e2e/home.ts
@@ -0,0 +1,7 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Home page', () => {
+  it('should render home sections', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
