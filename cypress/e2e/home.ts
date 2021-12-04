762cd26a44206541e870e0cdd4afad769264253c
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-04 17:26:39 -0300

    start getting banner with get, findByRole and within

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -3,5 +3,11 @@
 describe('Home page', () => {
   it('should render home sections', () => {
     cy.visit('/')
+
+    cy.get('.slick-slider').within(() => {
+        cy.findByRole('heading', { name: /cyberpunk 2077/i })
+        cy.findByRole('link', { name: /buy now/i })
+    })
   })
+
 })
