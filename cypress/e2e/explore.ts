7bca4732cc1887725e8b841794527aeb0630297b
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-05 17:26:52 -0300

    add tests to sort by price

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -3,11 +3,11 @@
 import { priceFields, genreFields, platformFields, sortFields} from '../../src/utils/filter/fields'
 
 describe('Explore Page', () => {
-  before(()=>{
+  before(() => {
     cy.visit('/games')
   })
-  it('should render filters columns', () => {
 
+  it('should render filters columns', () => {
     cy.findByRole('heading', { name: /sort by price/i }).should('exist')
     cy.findByRole('heading', { name: /^price/i }).should('exist')
     cy.findByRole('heading', { name: /platforms/i }).should('exist')
@@ -38,6 +38,46 @@ describe('Explore Page', () => {
 
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeGreaterThan(0)
+    })
+  });
+
+  it('should filter by price', () => {
+    cy.findByText(/highest to lowest/i).click()
+
+    cy.findByText(/free/i).click()
+    cy.location('href').should('contain', 'price_lte=0')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('$0.00').should('exist')
+    })
+
+    cy.findByText('Under $50').click()
+    cy.location('href').should('contain', 'price_lte=50')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(50)
+    })
+
+    cy.findByText('Under $100').click()
+    cy.location('href').should('contain', 'price_lte=100')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(100)
+    })
+
+    cy.findByText('Under $150').click()
+    cy.location('href').should('contain', 'price_lte=150')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(150)
+    })
+
+    cy.findByText('Under $250').click()
+    cy.location('href').should('contain', 'price_lte=250')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(250)
+    })
+
+    cy.findByText('Under $500').click()
+    cy.location('href').should('contain', 'price_lte=500')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(500)
     })
   });
 });
